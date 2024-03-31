c3ac3ed356bb7fc709b743a3466f885a7c746762
unknown <kotbekareem74@gmail.com>
2024-03-31 17:56:23 +0200

    edist

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -250,7 +250,7 @@ methods: {
     async getLatestTerms(lang){
         $('.loader').fadeIn().css('display', 'flex')
         try {
-            const response = await axios.post( `/latest-terms`, {
+            const response = await axios.post( `{{ route('term.getlatest') }}`, {
                 lang: lang
             },
             );
