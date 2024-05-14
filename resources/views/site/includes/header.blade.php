1963900dfad991dffb4530c11a0c60fd181bcfa4
kareem22T <kotbekareem74@gmail.com>
2024-05-14 22:06:15 +0300

    edist

--- resources/views/site/includes/header.blade.php
+++ resources/views/site/includes/header.blade.php
@@ -8,7 +8,7 @@
                 <input type="text" name="search" id="search" placeholder="Search for Term" v-model="search" @input="handleSearch">
                 <i class="fa fa-search" @click="handleSearch"></i>
                 <div class="suggestion" v-if="searchArticles.length && search" style="font-size: 17px;position: absolute;top: 100%;display: flex;flex-direction: column;background: white;width: 100%;border-radius: 10px;margin-top: 10px;">
-                    <a :href="`/term/${item.name}/${item.id}`"  v-for="item in searchArticles.slice(0, 5)" :key="item.id" style="font-size: 16px;border-bottom: 1px solid #80808052;padding: 5px 1rem;color: #1a3467;">@{{item.titles[0].title}}<span>@{{ item.category.names[0].name }}</span></a>
+                    <a :href="`/term/${item.name}/${item.id}`"  v-for="item in searchArticles.slice(0, 5)" :key="item.id" style="font-size: 16px;border-bottom: 1px solid #80808052;padding: 5px 1rem;color: #1a3467;">@{{item.names[0].name}}<span>@{{ item.category.names[0].name }}</span></a>
                     <a :href="`/search/${search}`" style="font-size: 16px;box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;padding: 5px 1rem;color: #1a3467;text-align: center;">Show All</a>
                 </div>
             </div>
