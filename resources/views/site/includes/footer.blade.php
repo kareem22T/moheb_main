099123ace3c3ca032f25b1b04bb40554c98d531e
kareem22T <kotbekareem74@gmail.com>
2024-07-11 22:34:05 +0300

    edits

--- resources/views/site/includes/footer.blade.php
+++ resources/views/site/includes/footer.blade.php
@@ -4,7 +4,7 @@
         <a href="">@{{ page_content ? page_content.footer.nav.link_2 : "Cookie Policy" }}</a>
         <a href="/contact-us">@{{ page_content ? page_content.footer.nav.link_3 : "Contact Us" }}</a>
     </div>
-    <div class="bottom">
+    <div class="bottom" style="flex-direction: column;display: flex">
         Powerd by Centre de paris 2024 <br>
         <div style="display: flex; gap: 8px">
             technology provider  <a style="color: #fff;" href="https://www.linkedin.com/in/omar-ldeeb" target="_blank">@omarldeeb</a>
