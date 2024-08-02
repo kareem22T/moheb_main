f81bdeab96ff128a2eee00c7245c18094c656648
kareem22T <m.othman@clicksegypt.net>
2024-08-03 00:16:40 +0300

    eids

--- resources/views/site/includes/footer.blade.php
+++ resources/views/site/includes/footer.blade.php
@@ -5,9 +5,9 @@
         <a href="/contact-us">@{{ page_content ? page_content.footer.nav.link_3 : "Contact Us" }}</a>
     </div>
     <div class="bottom" style="flex-direction: column;display: flex">
-        Powerd by Centre de paris 2024 <br>
+        Powerd by Centre De Paris 2024 <br>
         <div style="display: flex; gap: 8px">
-            technology provider  <a style="color: #fff;" href="https://www.linkedin.com/in/omar-ldeeb" target="_blank">@omarldeeb</a>
+            Technology Provider  <a style="color: #fff;" href="https://www.linkedin.com/in/omar-ldeeb" target="_blank">@omarldeeb</a>
         </div>
     </div>
 </footer>
